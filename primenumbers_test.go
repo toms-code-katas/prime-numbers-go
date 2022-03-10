9c3efa354dcf970b7fdf5a0b0377328d0bedd9ad
tom1299 <tom1299@jeeatwork.com>
2022-03-10 19:14:11 +0100

    Add table driven test case

--- primenumbers_test.go
+++ primenumbers_test.go
@@ -2,6 +2,7 @@ package primenumbers
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -11,6 +12,27 @@ func TestCalculatePrimes(t *testing.T) {
 	for i, prime := range primes {
 		if expected_primes[i] != prime {
 			t.Fatal(fmt.Sprintf("Expected primes %v and calculated primes %v are not equal", expected_primes, primes))
+		}
+	}
+}
+
+func TestCalculatePrimesUsingTable(t *testing.T) {
+
+	type test struct {
+		start  uint64
+		stop   uint64
+		primes []uint64
+	}
+
+	tests := []test{
+		{start: 1, stop: 10, primes: []uint64{2, 3, 5, 7}},
+		{start: 1, stop: 25, primes: []uint64{2, 3, 5, 7, 11, 13, 17, 19, 23}},
+	}
+
+	for _, tc := range tests {
+		calculatedPrimes := CalculatePrimes(tc.start, tc.stop)
+		if !reflect.DeepEqual(tc.primes, calculatedPrimes) {
+			t.Fatalf("expected: %v, got: %v", tc.primes, calculatedPrimes)
 		}
 	}
 }
