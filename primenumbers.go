e4eae358a611c40236e250667c4c468eea69374d
tom1299 <tom1299@jeeatwork.com>
2022-03-18 18:11:28 +0100

    Fix linting errors

--- primenumbers.go
+++ primenumbers.go
@@ -1,10 +1,11 @@
 package primenumbers
 
+// CalculatePrimes will calculate prime numbers between start and stop
 func CalculatePrimes(start uint64, stop uint64) []uint64 {
 	primes := make([]uint64, 0)
 
 	for number := start; number <= stop; number++ {
-		if IsPrimeToTen(number) {
+		if isPrimeToTen(number) {
 			primes = append(primes, number)
 		} else if number == 1 {
 			continue
@@ -28,6 +29,6 @@ func CalculatePrimes(start uint64, stop uint64) []uint64 {
 	return primes
 }
 
-func IsPrimeToTen(prime uint64) bool {
+func isPrimeToTen(prime uint64) bool {
 	return prime == 2 || prime == 3 || prime == 5 || prime == 7
 }
